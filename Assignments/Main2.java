89b382b2afab8429f6b4ce2c609fdb7cae7eaed6
raihacks <siddhirai326@gmail.com>
2025-12-09 16:55:02 +0530

    ex2

--- Assignments/Main2.java
+++ Assignments/Main2.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
-public class Main2{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number: ");
-        int num = sc.nextInt();
-        if(num % 2 == 0){
-            System.out.println("Number " + num + " is even.");
-        }
-        else{
-            System.out.println("Number " + num + " is odd.");
-        }
-    }
-}
+// public class Main2{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the number: ");
+//         int num = sc.nextInt();
+//         if(num % 2 == 0){
+//             System.out.println("Number " + num + " is even.");
+//         }
+//         else{
+//             System.out.println("Number " + num + " is odd.");
+//         }
+//     }
+// }
+
+// public class Main2{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Please enter your name: ");
+//         String name = sc.nextLine();
+//         System.out.println("Hello! " + name + "!");
+//     }
+// }
