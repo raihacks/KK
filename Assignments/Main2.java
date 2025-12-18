05e2b9c8e0655fca39417cc34b685dcd78d0fb77
raihacks <siddhirai326@gmail.com>
2025-12-18 18:58:59 +0530

    aa

--- Assignments/Main2.java
+++ Assignments/Main2.java
@@ -43,7 +43,7 @@ public class Main2{
     }
 }
 
-public class Main2{
-    public static void main(String[] args){
-    }
-}
+// public class Main2{
+//     public static void main(String[] args){
+//     }
+// }
